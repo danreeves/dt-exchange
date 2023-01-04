c9b5f02f8306c0b1531b833a2beef5eb01418f0d
Dan Reeves <hey@danreev.es>
2023-01-04 16:01:02 +0000

    Move build to gh actions (#30)

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-  // src/background.ts
-  var ext = chrome || browser;
-  var dashboardUrl = "https://accounts.atoma.cloud/dashboard";
-  ext.browserAction.onClicked.addListener(() => {
-    ext.tabs.create({ url: dashboardUrl });
-  });
-})();
