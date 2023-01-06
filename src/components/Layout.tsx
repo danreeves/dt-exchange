5d6c28ec8fd90658790840105bc2d47557be95a3
Chris-Bitler <chris@cbitler.io>
2023-01-06 15:29:06 -0800

    Use site-specific permissions and clarify expiration timer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Button } from "./Button"
 import { archetype } from "../icons"
 import { Loading } from "./Loading"
