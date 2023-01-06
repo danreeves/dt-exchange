6958370863f56223412f437bfd5929d7db5eae70
Coconutcoo <10isaacc@gmail.com>
2023-01-06 12:51:57 +0000

    Fixed tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,30 +1,19 @@
 import test from "ava"
-import { Buffer } from "buffer/"
 import { safeUserParse } from "./utils"
 
 let userNames = [
-  `【パンツ】Pantsu`,
-  `🎃 ASH  П (◣_◢) П  🎃`,
-  `七曜曜`,
-  `Dunesca ʕ•͡ᴥ•ʔ`,
+  `【パンツ】Pantsu#1234`,
+  `🎃 ASH  П (◣_◢) П  🎃#1234`,
+  `七曜曜#1234`,
+  `Dunesca ʕ•͡ᴥ•ʔ#1234`,
+  `Deadsiesthe•̪̀●́#1234`,
+  `Deadsiesthe"*�#1234`
 ]
 
-function toB64(str: string): string {
-  return Buffer.from(str, "ascii").toString("base64")
-}
-
-function fromB64(str: string): string {
-  return Buffer.from(str, "base64").toString()
-}
-
-function encode(obj: Record<string, unknown>): string {
-  return fromB64(toB64(JSON.stringify(obj)))
-}
-
 for (let user of userNames) {
-  test(`safeUserParse can decode special user names: ${user}`, (t) => {
+  test(`safeUserParse can parse special user names: ${user}`, (t) => {
     const TestAccessToken = "This Should Be Returned"
-    let userString = encode({ AccessToken: TestAccessToken, AccountName: user })
+    let userString = `{\"AccessToken\": \"${TestAccessToken}\", \"AccountName\":\"${user}\"}`
     let decoded = safeUserParse(userString)
     t.not(decoded, undefined)
     t.is(decoded!.AccessToken, TestAccessToken)
@@ -38,20 +27,21 @@ for (let user of userNames) {
 
 test(" safely removes bad usernames anywhere in json", (t) => {
   const TestAccessToken = "This Should Be Returned"
-  const user = userNames[0]
-  let strings = [
-    encode({ AccountName: user }),
-    encode({ AccessToken: TestAccessToken, AccountName: user }),
-    encode({ AccountName: user, AccessToken: TestAccessToken }),
-    encode({ AccessToken: TestAccessToken, AccountName: user, RefreshToken: TestAccessToken }),
-  ]
-  for (let str of strings) {
-    let decoded = safeUserParse(str)
-    t.not(decoded, undefined)
-    t.not(decoded?.AccountName, undefined)
-    t.not(decoded?.AccountName, '')
-    if (decoded?.AccountName) {
-      t.is(/[\w-#]+/.test(decoded?.AccessToken), true)
+  for (let user of userNames) {
+    let strings = [
+      `{\"AccountName\": \"${user}\"}`,
+      `{\"AccessToken\":\"${TestAccessToken}\", \"AccountName\":\"${user}\"}`,
+      `{\"AccountName\":\"${user}\", \"AccessToken\":\"${TestAccessToken}\"}`,
+      `{\"RefreshToken\":\"${TestAccessToken}\", \"AccountName\":\"${user}\", \"AccessToken\":\"${TestAccessToken}\"}`
+    ]
+    for (let str of strings) {
+      let decoded = safeUserParse(str)
+      t.not(decoded, undefined)
+      t.not(decoded?.AccountName, undefined)
+      t.not(decoded?.AccountName, '')
+      if (decoded?.AccountName) {
+        t.is(/[\w-#]+/.test(decoded?.AccessToken), true)
+      }
     }
   }
 })
